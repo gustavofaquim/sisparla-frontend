1a890fe85e16482b3e1f1fdf8975d935d10d6b84
Gustavo Faquim <gustavofaquim408@gmail.com>
2023-10-09 19:31:48 -0300

    feat: implementando novo menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,27 +2,51 @@ import React from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
 
+import "../styles/components/sidebar.sass"
+
+
+import { 
+    FaTimes, 
+    FaHome, 
+    FaEnvelope, 
+    FaRegSun, 
+    FaUserAlt, 
+    FaIdCardAlt, 
+    FaRegFileAlt,
+    FaRegCalendarAlt,
+    FaChartBar,
+    FaCalendarAlt,
+    FaRegWindowClose
+  } from 'react-icons/fa';
+
 const Sidebars = () => {
   return (
-    <Sidebar>
-        <Menu>
-            <SubMenu prefix="🔥" label="Apoidores">
-                <MenuItem component={<Link to="/apoiadores" />}> Lista</MenuItem>
-                <MenuItem component={<Link to="/novo-apoiador" />}> Novo</MenuItem>
-                <MenuItem component={<Link to="/aniversariantes" />}> Aniversariantes</MenuItem>
-            </SubMenu>
+    <div className='sidebar-menu'>
+        <Sidebar>
+            <Menu>
+                <SubMenu  icon={<FaUserAlt/>} label="Apoidores">
+                    <MenuItem component={<Link to="/apoiadores" />}> Lista</MenuItem>
+                    <MenuItem component={<Link to="/novo-apoiador" />}> Novo</MenuItem>
+                    <MenuItem component={<Link to="/aniversariantes" />}> Aniversariantes</MenuItem>
+                    <MenuItem component={<Link to="/pendentes" />}> Pendetes</MenuItem>
+                </SubMenu>
 
-            <SubMenu label="Demandas">
-                <MenuItem> Google maps</MenuItem>
-                <MenuItem> Open street maps</MenuItem>
-            </SubMenu>
+                <SubMenu icon={<FaChartBar/>} label="Demandas">
+                    <MenuItem component={<Link to="/nova-demanda" />}> Cadastrar Nova</MenuItem>
+                    <MenuItem component={<Link to="/demandas " />}> Demandas</MenuItem>
+                    <MenuItem component={<Link to="/categorias " />}> Categorias</MenuItem>
+                    <MenuItem component={<Link to="/nova-categoria" />}>Nova Categorias</MenuItem>
+                </SubMenu>
 
-            <SubMenu label="Theme">
-                <MenuItem> Dark</MenuItem>
-                <MenuItem> Light</MenuItem>
-            </SubMenu>
-        </Menu>
-    </Sidebar>
+                <SubMenu icon={<FaCalendarAlt/>} label="Eventos">
+                    <MenuItem component={<Link to="/novo-evento" />}> Cadastrar Evento</MenuItem>
+                    <MenuItem component={<Link to="/eventos " />}> Eventos</MenuItem>
+                </SubMenu>
+
+                
+            </Menu>
+        </Sidebar>
+    </div>
     );
 };
 
