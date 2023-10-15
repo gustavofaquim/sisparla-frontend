be41ffc0e33f2e9126a7c4317e6e9430c9b01d59
Gustavo Faquim <gustavofaquim408@gmail.com>
2023-10-15 19:38:51 -0300

    feat: implementando ficha de apoiador

--- src/components/ApoiadoresFicha.jsx
+++ src/components/ApoiadoresFicha.jsx
@@ -1,11 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import userFetch from "../axios/config.js";
 
 
-const getApoiador = async(id) => {
 
-}
+const ApoiadoresFicha = () => {
+
+    const params = useParams();
+    const id = params.id;
+
+    const navigate = useNavigate();
 
 
-const ApoiadoresFicha = (id) => {
+    const [data, setData] = useState({
+        title: '',
+        description: '',
+        image: '',
+        profissao: '',
+        cpf: '',
+        religiao: '',
+        nascimento: '',
+        classificacao: '',
+        email: '',
+        telefone: '',
+        situacao: '',
+        cep: '',
+        cidade: '',
+        estado: '',
+        lagradouro: '',
+        quadra: '',
+        numero: '',
+        bairro: '',
+        pontoReferencia: '',
+        partido: '',
+        entidade: '',
+        informacoesAdicionais: ''
+    });
+
+    const getApoiador = async() => {
+        
+        if(id === undefined){
+            console.log("Apoiador não encontrado");
+            return;
+        }
+
+
+        await userFetch.get(`/apoiadores/${id}`)
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((error) => {
+
+                if(error.response){
+                    console.log(error.response.data.msg);
+                }else{
+                    console.log("Api não respondeu");
+                }
+            });
+    }
+
+    useEffect(() => {
+        getApoiador();
+    }, []);
+    
+
     return(
         <div className="apoiador-ficha">
             <h3>Ficha de Cadastro</h3>
