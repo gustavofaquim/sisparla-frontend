2c72607de689bb9febe06db7cc801acc8bec6931
Gustavo Faquim <gustavofaquim408@gmail.com>
2023-10-24 18:56:55 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/components/ApoiadoresEdit.jsx
+++ src/components/ApoiadoresEdit.jsx
@@ -218,7 +218,11 @@ const ApoiadoresEdit = () => {
                             <option >Escolher...</option>
                             {
                                 religioes.map((religiao) => (
+<<<<<<< Updated upstream
 
+=======
+                                    
+>>>>>>> Stashed changes
                                     <option key={religiao.IdReligiao} selected={religiao.Nome === data.religiao}  value={religiao.Nome}>{religiao.Nome}</option>
                                 ))
                             }
