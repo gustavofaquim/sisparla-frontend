945243044dd2685de38668aae1d612f6316aa73f
Gustavo Faquim <gustavofaquim408@gmail.com>
2024-02-28 10:42:17 -0300

    feat: inserido tratamento de exceção no  AuthProvider

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,40 +1,47 @@
 // AuthProvider.jsx
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import {jwtDecode} from 'jwt-decode';
-
 import userFetch from './axios/config.js';
+
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  
   const [user, setUser] = useState(null);
+  
 
   useEffect(() => {
-  
     const token = sessionStorage.getItem('token');
   
     if (token && !user) {
-      const decodedUser = jwtDecode(token);
-      setUser(decodedUser);
+      try {
+        const decodedUser = jwtDecode(token);
+        setUser(decodedUser);
+      } catch (error) {
+        console.error('Erro ao decodificar o token: ', error);
+        // Adote o comportamento adequado em caso de erro na decodificação
+      }
     }
   }, [user]);
 
   const handleLogout = async() => {
    
-    const response = await userFetch.delete(`/logout`)
+    try {
+      
+      const response = await userFetch.delete(`/logout`)
     
-    console.log(response)
-
-    if(response.status === 200){
-      console.log('Até mais ver...')
+      if(response.status == 200){
+        console.log('Até mais ver...')
+        sessionStorage.removeItem('token');
+        setUser(null);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log('Erro ao realizar logout: ' + error)
       sessionStorage.removeItem('token');
       setUser(null);
       window.location.reload();
-      navigate('/login');
-      
-    }
-    else if(response.status === 401){
-      console.log('deu erooo aquiiii')
     }
    
 
