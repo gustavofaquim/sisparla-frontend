7ecb549841eba3abf02765b779634a4315b8e8a1
Gustavo Faquim <42013717+gustavofaquim@users.noreply.github.com>
2024-03-23 10:06:01 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  //base: 'http://52.90.11.58/',
-  plugins: [react()],
-})
